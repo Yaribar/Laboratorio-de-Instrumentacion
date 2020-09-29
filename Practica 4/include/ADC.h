b563e94a70ad9b907025c29ef455828236b4d6fb
Yaribar <40440445+Yaribar@users.noreply.github.com>
2020-09-29 18:14:08 -0500

    Creacion de dos clases

---
+++ Practica 4/include/ADC.h
@@ -0,0 +1,29 @@
+#ifndef _ADC_h
+#define _ADC_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+
+class ADC{
+public:
+	ADC(uint8_t pin, uint8_t vref, uint8_t bits_resolution, uint16_t raw_voltage);
+	~ADC();
+
+	private:
+	
+};
+
+class RGB{
+public:
+	RGB(uint8_t red, uint8_t green, uint8_t blue, uint8_t intensity);
+	~RGB();
+
+
+private:
+	
+};
