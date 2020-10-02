ae7068e1ca02d15af47c8e2988a67681393408a4
Yaribar <40440445+Yaribar@users.noreply.github.com>
2020-10-02 01:37:17 -0500

    LED, AVG

---
+++ Practica 4 Laboratorio/Practica 4/include/Comulative_AVG.h
@@ -0,0 +1,21 @@
+#ifndef _RGB_LED_h
+#define _RGB_LED_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class AVG{
+public:
+    AVG();
+    ~AVG();
+    
+
+private:
+
+
+};
+
+#endif
