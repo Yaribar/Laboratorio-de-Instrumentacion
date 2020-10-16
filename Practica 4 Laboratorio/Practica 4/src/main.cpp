0d6473b57b8bf54725d80c6ec8d7a2cfcb773c46
Yaribar <40440445+Yaribar@users.noreply.github.com>
2020-10-16 13:28:56 -0500

    Envio de datos con promedio

--- Practica 4 Laboratorio/Practica 4/src/main.cpp
+++ Practica 4 Laboratorio/Practica 4/src/main.cpp
@@ -1,38 +1,59 @@
 #include <Arduino.h>
 #include "ADC_Read.h"
-#include <BluetoothSerial.h>
 #include "Comulative_AVG.h"
 #include "RGB.h"
 
 #define PIN_RED 32
 #define PIN_GREEN 33
-#define PIN_BLUE 34
-#define RESOLUTION 10
-#define VREF 3.3f
-#define SAMPLING_PERIOD 5
+#define PIN_BLUE 25
+#define RESOLUTION 12
+#define VREF 3.3
 
-ADCRead ADC(36, RESOLUTION, VREF);
+ADCRead ADC(27, RESOLUTION, VREF);
 AVG AVG_Red,AVG_Green,AVG_Blue;
-BluetoothSerial SerialBT;
 RGB RGB1(PIN_RED,PIN_GREEN,PIN_BLUE);
 
-String on_led_time;
+uint8_t on_led_time=250;
+uint8_t state=RED;
 unsigned long start_time;
+String data_in;
 
 void setup() {
-  SerialBT.begin("Wano");
   Serial.begin(115200);
-  start_time=0;
+  start_time=millis();
+  RGB1.setRGB(RED);
 }
 
 void loop() {
+  if((millis() - start_time) > on_led_time){
+    switch(state){
+      case RED:
+      AVG_Red.avg(ADC.readRaw());
+      state = GREEN;
+      RGB1.setRGB(GREEN);
+      break;
+      
+      case GREEN:
+      AVG_Green.avg(ADC.readRaw());
+      state = BLUE;
+      RGB1.setRGB(BLUE);
+      break;
 
+      case BLUE:
+      AVG_Blue.avg(ADC.readRaw());
+      state = RED;
+      Serial.printf("%.1f,%.1f,%.1f\n",AVG_Red.getCurrentAvg(),AVG_Green.getCurrentAvg(),AVG_Blue.getCurrentAvg());
+      RGB1.setRGB(RED);
+      break;
+    }
+    start_time = millis();
+  }
   
-  printf("%.1f,%.1f,%.1f \n",255.1,255.1,255.1);
-  delay(300);
-  //if (millis() - start_time > SAMPLING_PERIOD) {
-		//start_time = millis();
-    //SerialBT.printf("%.1f,%.1f,%.1f \n",AVG_Red.getCurrentAvg(),AVG_Green.getCurrentAvg(),AVG_Blue.getCurrentAvg());
-    //SerialBT.printf("%.1f,%.1f,%.1f \n",255.1,255.1,255.1);       
-  //}  
+  if (Serial.available()){
+    data_in = Serial.readStringUntil('\n');
+    on_led_time = data_in.toInt();
+    AVG_Red.resetAvg();
+    AVG_Green.resetAvg();
+    AVG_Blue.resetAvg();
+  }
 }
