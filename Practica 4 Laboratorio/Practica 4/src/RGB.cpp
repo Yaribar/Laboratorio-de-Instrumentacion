d234641cab6c22f0fcd95af2c673128ed9c35b6e
Yaribar <40440445+Yaribar@users.noreply.github.com>
2020-10-16 13:27:44 -0500

    RGB anodo comun

--- Practica 4 Laboratorio/Practica 4/src/RGB.cpp
+++ Practica 4 Laboratorio/Practica 4/src/RGB.cpp
@@ -4,13 +4,13 @@
 
 RGB::RGB(uint8_t pin_red, uint8_t pin_green, uint8_t pin_blue)
 {
-    _pin_red=pin_red;
-    _pin_green=pin_green;
-    _pin_blue=pin_blue;
+  _pin_red=pin_red;
+  _pin_green=pin_green;
+  _pin_blue=pin_blue;
 
-    PWM_Red.setup(_pin_red, 0, 10000, _bits_resolution, true);
-	PWM_Green.setup(_pin_green, 1, 10000, _bits_resolution, true);
-    PWM_Blue.setup(_pin_blue, 3, 10000, _bits_resolution, true);
+  PWM_Red.setup(_pin_red, 1, 10000, _bits_resolution, true);
+	PWM_Green.setup(_pin_green, 2, 10000, _bits_resolution, true);
+  PWM_Blue.setup(_pin_blue, 3, 10000, _bits_resolution, true);
 	
 }
 
@@ -20,28 +20,30 @@ RGB::~RGB()
 }
 void RGB::setRGB(float intensity_red,float intensity_green, float instensity_blue)
 {
+    
     PWM_Red.setDuty(intensity_red);
     PWM_Green.setDuty(intensity_green);
     PWM_Blue.setDuty(instensity_blue);
+    
 }
 
 void RGB::setRGB(byte state){
     switch (state)
   {
   case RED:
-    setRGB(100,0,0);
+    setRGB(0,100,100);
     break;
   case GREEN:
-    setRGB(0,100,0);
+    setRGB(100,0,100);
     break;
   case BLUE:
-    setRGB(0,0,100);
+    setRGB(100,100,0);
     break;
   case WHITE:
-    setRGB(100,100,100);
+    setRGB(0,0,0);
     break;
   case BLACK:
-    setRGB(0,0,0);
+    setRGB(100,100,100);
 
     break;  
   default:
