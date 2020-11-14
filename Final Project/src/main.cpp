564ffe1f6f831a42f29e5f265fd558184c07b04b
Yaribar <40440445+Yaribar@users.noreply.github.com>
2020-11-13 23:33:06 -0600

    Lectura de encoder y phototransistor dos veces

--- Final Project/src/main.cpp
+++ Final Project/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
 const byte encoderPin = 25;
+const byte picturePin = 26;
 volatile uint8_t interruptCounter = 0;
+volatile uint8_t interruptCounter2 = 0;
 uint8_t numberOfInterrupts = 0;
 uint rev=0;
+uint32_t tiempo=0;
+float speed=0;
+uint8_t pulse=0;
+int contador = 0;
+bool dato = LOW;
+bool datoAnterior = LOW;
  
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
  
@@ -12,14 +20,14 @@ void IRAM_ATTR encoderInterrupt() {
   interruptCounter++;
   portEXIT_CRITICAL_ISR(&mux);
 }
- 
+
 void setup() {
  
   Serial.begin(115200);
-  Serial.println("Monitoring interrupts: ");
   pinMode(encoderPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(encoderPin), encoderInterrupt, FALLING);
- 
+  pinMode(picturePin,INPUT_PULLUP);
+  
 }
  
 void loop() {
@@ -34,11 +42,22 @@ void loop() {
       Serial.print("An interrupt has occurred. Total: ");
       Serial.println(numberOfInterrupts);
   }
+
+  
+dato = digitalRead(26);
+ if (dato == HIGH && datoAnterior == LOW)
+ {
+  Serial.println("Pic");
+   }
+ datoAnterior = dato;
+
   if(numberOfInterrupts==228){
     rev++;
     Serial.println(rev);
     numberOfInterrupts=0;
     }
+
+    
 }
 
 
