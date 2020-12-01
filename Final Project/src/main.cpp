fc69fee3952698db21592ab6b1a27805b3f3fb01
Yaribar <40440445+Yaribar@users.noreply.github.com>
2020-12-01 17:55:33 -0600

    + PID + SERVO libraries

--- Final Project/src/main.cpp
+++ Final Project/src/main.cpp
@@ -3,9 +3,12 @@
 #include "NiMOV.h"
 #include "IR.h"
 #include "ENCODER.h"
+#include "ESP_SERVO.h"
+
 
 const byte encoderPin = 25;
 const byte picturePin = 26;
+const byte servoPin = 32;
 volatile uint8_t interruptCounter = 0;
 uint8_t numberOfInterrupts = 0;
 uint rev=0;
@@ -18,6 +21,8 @@ float data[2];
 
 IR IR(26);
 NiMOV HBRIDGE(AIN1,0, AIN2, 1,10000, 10,100);
+ESPServo Servo1;
+
 
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
  
@@ -33,7 +38,8 @@ void setup() {
   pinMode(encoderPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(encoderPin), encoderInterrupt, FALLING);
   pinMode(picturePin,INPUT_PULLUP);
-  
+  Servo1.setup(servoPin,2);
+   
 }
  
 void loop() {
@@ -41,7 +47,9 @@ void loop() {
   if (Serial.available()) {
 		input = Serial.readStringUntil('\n');
 		parseString(input, ",", data);
-		Serial.printf("%.1f \r\n", data[0]);
+		Serial.printf("%.1f,%.1f\r\n", data[0],data[1]);
+    Servo1.write((int)data[1]);
+    delay(200);  
 	}
  
   if(IR.pulse()){
@@ -56,7 +64,7 @@ void loop() {
       portEXIT_CRITICAL(&mux);
  
       numberOfInterrupts++;
-      Serial.print("An interrupt has occurred. Total: ");
+      Serial.print("Interrupt Total: ");
       Serial.println(numberOfInterrupts);
   }
 
@@ -66,8 +74,10 @@ void loop() {
     numberOfInterrupts=0;
     }
 
-  if(numberOfInterrupts==131)
+  if(numberOfInterrupts==10){
     HBRIDGE.setStop(0);
+    
+  }
 }
 
 
